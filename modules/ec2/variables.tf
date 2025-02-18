5b47cf404fa8d3043682d981d02c32a964c9d112
Vikram Singh <128373726+vikramsingh998@users.noreply.github.com>
2025-02-18 14:23:33 +0530

    Update variables.tf
    edit

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,11 +1,6 @@
 # modules/ec2/variables.tf
 variable "ami_id" {}
 variable "instance_type" {}
-variable "key_name" {}
 variable "security_group_ids" { type = list(string) }
 variable "subnet_id" {}
-variable "volume_size" { default = 20 }
-variable "volume_type" { default = "gp2" }
-variable "associate_public_ip" { default = false }
 variable "tags" { type = map(string) }
-
